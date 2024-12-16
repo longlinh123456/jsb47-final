0ac41478a075d33d48e12f364932c1bc6f31aa52
longlinh123456 <buiduylinh700@gmail.com>
2024-12-16 23:21:31 +0700

    use saved images

--- shop.js
+++ shop.js
@@ -2,55 +2,55 @@
 const catalog = {
 	"Bút chì": [
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220810/22073372_thumb.JPG",
-			title: "Bút chì khúc Hello Kitty",
+			image: "https://cf.shopee.vn/file/4ac06c800f9bac834e96377017087b3e",
+			title: "Bút chì đen 2B thân vàng Gstar",
 			price: "15000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220810/22073371_thumb.JPG",
-			title: "Bút chì khúc Khủng long dinosaur đội mũ party",
+			image: "https://cf.shopee.vn/file/3dcd8374c61634a75965e88592494124",
+			title: "Bút chì kim bấm Nhật A255",
 			price: "15000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220810/22073370_thumb.JPG",
-			title: "Bút chì khúc Unicorn sweet dream",
+			image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcf.shopee.vn%2Ffile%2F1708553d1f1c169450953084b956879f&f=1&nofb=1&ipt=8200bf1d7efe110c07d706343496608a6b3cf1838cc080ca26ae21210bd439cc&ipo=images",
+			title: "Bút chì gỗ Uni Mitsubishi 9800",
 			price: "15000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220715/22070121_thumb.JPG",
-			title: "Bút chì MJ vĩnh cửu Rabbit new world",
+			image: "https://product.hstatic.net/1000039248/product/5_2ec4d771f84c4d71803a3c19d6d8c074_1024x1024_b607023c01204046a9c6e4f9e0c9a969_1024x1024.jpg",
+			title: "Bút chì bấm Thiên Long PC-026",
 			price: "15000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220718/22070120_thumb.JPG",
-			title: "Bút chì MJ vĩnh cửu I love duck",
+			image: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-loh1knbf1jg708",
+			title: "Bút chì gỗ 2B Thiên Long FO-GP02",
 			price: "15000"
 		}
 	],
 	"Hộp bút": [
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220806/22070719_thumb.JPG",
-			title: "Hộp bút MJ Rabbit carrot planet heart line 8x19",
+			image: "https://salt.tikicdn.com/cache/280x280/ts/product/cd/1b/0f/fc97fac13935187ff46604ff2e94dd18.png",
+			title: "Hộp bút phiên bản Mùa Thu Vàng",
 			price: "60000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220806/22070718_thumb.jpg",
-			title: "Hộp bút MJ Baby bear I am happy 4x8x20",
+			image: "https://down-vn.img.susercontent.com/file/a44f0ab6c518a9c6d0000385b4bae7df",
+			title: "Hộp bút nhiều ngăn đa năng",
 			price: "60000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220806/22070615_thumb.JPG",
-			title: "Hộp bút lớn Hello I'm bear 7x11x22",
+			image: "https://cf.shopee.vn/file/2173abbb3901655b1116e44b33be59d7",
+			title: "Hộp bút nhựa nam châm 2 mặt",
 			price: "120000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220802/22070402_thumb.JPG",
-			title: "Hộp bút lớn Rabbit flower fruit 5x12x21",
+			image: "https://salt.tikicdn.com/ts/tmp/c5/29/af/b2c2619e24912f0b440bfc6a13b15ab8.jpg",
+			title: "Hộp bút vuông 2 dây kéo",
 			price: "120000"
 		},
 		{
-			image: "https://img.cdn.vncdn.io/nvn/ncdn/store/7534/ps/20220802/22070401_thumb.JPG",
-			title: "Hộp bút MJ Sheep good night 3x8x21",
+			image: "https://cf.shopee.vn/file/97e9ad2f20f5e2d036faccd9eb50595e",
+			title: "Hộp bút nhựa trong suốt",
 			price: "80000"
 		},
 	]
